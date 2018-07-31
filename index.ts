89d4971c31a61ca8e8747d74ef823dc0f06e74a2
Nikki Ashton <na@boligportal.dk>
2018-07-31 14:09:51 +0200

    Add delete all functionality

--- index.ts
+++ index.ts
@@ -111,6 +111,22 @@ export class PostgresRecordProvider {
     await this.pool.query(`DELETE FROM ${collectionName} WHERE _id = $1`, [ identifier ]);
   };
 
+    /**
+   * Delete all collections.
+   */
+  public async deleteAll() {
+    await this.pool.query(`DO $$ DECLARE
+    r RECORD;
+  BEGIN
+    -- if the schema you operate on is not "current", you will want to
+    -- replace current_schema() in query with 'schematodeletetablesfrom'
+    -- *and* update the generate 'DROP...' accordingly.
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
+        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+    END LOOP;
+  END $$;`);
+  }
+
   /**
    * If missing table exception, create table and rerun query
    * @param ex 
